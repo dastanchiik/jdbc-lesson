cf5701f4e2d07efbe7b4a56786b55146fc852c48
CompMir.kg <dosyafree32@gmail.com>
2024-08-28 16:46:22 +0600

    now I'm use method getAll for Country class

--- src/main/java/it/App.java
+++ src/main/java/it/App.java
@@ -15,20 +15,20 @@ import java.sql.SQLException;
 public class App {
     public static void main(String[] args) throws SQLException {
         CountryRepository repository = new CountryRepository();
-        Country country = new Country();
-        country.setTitle("Brazil");
-        country.setPopulation(898765434);
-        repository.saveCountry(country);
-        Country country1 = new Country();
-        country1.setTitle("Sweden");
-        country1.setPopulation(844752494);
-        repository.saveCountry(country1);
-
-        Country country2 = new Country();
-        country2.setTitle("Kanada");
-        country2.setPopulation(993715434);
-        repository.saveCountry(country2);
-//        System.out.println(repository.getAllCity());
+//        Country country = new Country();
+//        country.setTitle("Brazil");
+//        country.setPopulation(898765434);
+//        repository.saveCountry(country);
+//        Country country1 = new Country();
+//        country1.setTitle("Sweden");
+//        country1.setPopulation(844752494);
+//        repository.saveCountry(country1);
+//
+//        Country country2 = new Country();
+//        country2.setTitle("Kanada");
+//        country2.setPopulation(993715434);
+//        repository.saveCountry(country2);
+        System.out.println(repository.getAllCity());
 
     }
 }
