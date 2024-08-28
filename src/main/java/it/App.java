f698da14cd5b3df694b0d6283521373dc824cf77
CompMir.kg <dosyafree32@gmail.com>
2024-08-28 16:50:44 +0600

    I'm getting ready for use method getAllCity

--- src/main/java/it/App.java
+++ src/main/java/it/App.java
@@ -14,21 +14,20 @@ import java.sql.SQLException;
  */
 public class App {
     public static void main(String[] args) throws SQLException {
-        CountryRepository repository = new CountryRepository();
-//        Country country = new Country();
-//        country.setTitle("Brazil");
-//        country.setPopulation(898765434);
-//        repository.saveCountry(country);
-//        Country country1 = new Country();
-//        country1.setTitle("Sweden");
-//        country1.setPopulation(844752494);
-//        repository.saveCountry(country1);
+//        City city = new City();
+//        city.setTitle("Roma");
 //
-//        Country country2 = new Country();
-//        country2.setTitle("Kanada");
-//        country2.setPopulation(993715434);
-//        repository.saveCountry(country2);
-        System.out.println(repository.getAllCity());
+//        City city1 = new City();
+//        city1.setTitle("Madrid");
+//
+//        City city2 = new City();
+//        city2.setTitle("Tokyo");
+//
+        CityRepository repository = new CityRepository();
+        repository.createTable();
+//        repository.saveCity(city);
+//        repository.saveCity(city1);
+//        repository.saveCity(city2);
 
     }
 }
