cebc3a3c24218fa4e9c27b3af2fe6fa8bee410d1
CompMir.kg <dosyafree32@gmail.com>
2024-08-28 17:09:36 +0600

    Aaaah I'm stupid before save any data I should create table, I remember this and did it

--- src/main/java/it/App.java
+++ src/main/java/it/App.java
@@ -14,25 +14,26 @@ import java.sql.SQLException;
  */
 public class App {
     public static void main(String[] args) throws SQLException {
-        Measures measures = new Measures();
-        measures.setName("Richarlson");
-        measures.setAge(28);
-        measures.setOwnCity("Sao Paulo");
-
-        Measures measures1 = new Measures();
-        measures1.setName("Beckham");
-        measures1.setAge(49);
-        measures1.setOwnCity("London");
-
-        Measures measures2 = new Measures();
-        measures2.setName("Hazard");
-        measures2.setAge(36);
-        measures2.setOwnCity("Antwerp");
-
+//        Measures measures = new Measures();
+//        measures.setName("Richarlson");
+//        measures.setAge(28);
+//        measures.setOwnCity("Sao Paulo");
+//
+//        Measures measures1 = new Measures();
+//        measures1.setName("Beckham");
+//        measures1.setAge(49);
+//        measures1.setOwnCity("London");
+//
+//        Measures measures2 = new Measures();
+//        measures2.setName("Hazard");
+//        measures2.setAge(36);
+//        measures2.setOwnCity("Antwerp");
+//
+//        MeasureRepository repository = new MeasureRepository();
+//        repository.saveCountry(measures);
+//        repository.saveCountry(measures1);
+//        repository.saveCountry(measures2);
         MeasureRepository repository = new MeasureRepository();
-        repository.saveCountry(measures);
-        repository.saveCountry(measures1);
-        repository.saveCountry(measures2);
-
+        repository.createTable();
     }
 }
